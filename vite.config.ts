d2494412d4cfe9ad20b54b6b0f9399982899bfb4
Patroll <6214736+P4tr0ll@users.noreply.github.com>
2024-08-23 17:48:54 +0200

    Fix build base path and make it prod only

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "./vrc-event-poster-creator/",
-  plugins: [react()],
-  build: {
-    outDir: "./docs",
-  },
+export default defineConfig(({ command }) => {
+  const isProd = command === "build";
+
+  return {
+    ...(isProd && {
+      base: "/vrc-event-poster-creator/",
+    }),
+    plugins: [react()],
+    build: {
+      outDir: "./docs",
+    },
+  };
 });
